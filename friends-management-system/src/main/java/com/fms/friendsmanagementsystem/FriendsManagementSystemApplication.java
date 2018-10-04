c6ee7c5e8e5bdd46f60ac87d9db5500903fa55e0
swapnil.suhane <swapnil.suhane@lazada.com>
2018-10-04 15:37:52 +0800

    added new missing files

---
+++ friends-management-system/src/main/java/com/fms/friendsmanagementsystem/FriendsManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.fms.friendsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsManagementSystemApplication.class, args);
+    }
+}
